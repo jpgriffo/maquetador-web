7e41dedb7b52872e99162b53b6a0c3748aa5ccd2
Javier Perez-Griffo <javier@besol.es>
2012-07-30 13:01:40 +0200

    Inclusion of Rails 3.2 skeleton project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MaquetadorWeb::Application.config.secret_token = '0379c2b82107848f9ab155305898820c29241a0d1432e64c0c6abdddcf266968016a8259902dad614a1657865d8612fdd74d9a5eff880490bf00917526548fa8'
